6ed2d8d62ca08488b36ff9f456db68be6b18c409
Paulo Prescendo <paulovinicius1994@hotmail.com>
2019-06-18 12:01:20 -0300

    coding style to airbnb and add helmet and morgan libs

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(
-  "mongodb+srv://dbUser:dbUser@cluster0-epyzo.mongodb.net/test?retryWrites=true&w=majority",
+  'mongodb+srv://dbUser:dbUser@cluster0-epyzo.mongodb.net/test?retryWrites=true&w=majority',
   {
-    useNewUrlParser: true
-  }
+    useNewUrlParser: true,
+  },
 );
 
 app.use((req, res, next) => {
   req.io = io;
-  res.set("X-Powered-By", "");
   next();
 });
 
 app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.disable('X-Powered-By');
 
 app.use(
-  "/files",
-  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized')),
 );
 
-app.use(require("./routes"));
+app.use(require('./routes'));
 
 server.listen(3333);
